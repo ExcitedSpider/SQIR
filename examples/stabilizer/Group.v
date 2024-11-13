849e0bd571f6fad21b3b40fac733618aae5c241a
Chew F <360643820@qq.com>
2024-11-13 16:36:10 +1100

    feat: group

---
+++ examples/stabilizer/Group.v
@@ -0,0 +1,20 @@
+(*
+The group interfaces and lemmas. 
+Replace with MathComp Fingroup later
+*)
+
+Module Group.
+
+Record group: Type := {
+  A : Type;
+  op: A -> A -> A;
+  sym: A -> A;
+  e: A;
+  e_neutral_left : forall x:A, op e x = x;
+  sym_op: forall x y z: A, op (op x y) z = op x (op y z);
+  op_comm: forall x y: A, op x y = op y x
+}.
+
+End Group.
+
+
