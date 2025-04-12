772ea8127e37708fe9d6b8bd4a6a119385955f74
Chew F <chew.y.feng@outlook.com>
2025-04-12 17:27:20 +1000

    feat: p1g actions

--- examples/stabilizer/mathcomp/action.v
+++ examples/stabilizer/mathcomp/action.v
@@ -16,7 +16,7 @@ Import GroupScope.
 (* Definition left_injective op := forall x, injective (op^~ x). *)
 (* End ok. *)
 
-Section QuantumAction.
+Section ActionDef.
 
 Variables (aT : finGroupType) (D : {set aT}) (m n: nat).
 (* we restraint the applied set to be the hilbert space *)
@@ -46,6 +46,61 @@ Record action := Action {
   _ : is_action act
 }.
 
-End QuantumAction.
+End ActionDef.
+
+Require Import PauliGroup.
+Require Import P1Props.
+
+Section QuantumActions. 
+
+Import P1Group.
+Import P1GGroup.
+
+(* Apply a single-qubit pauli operator *)
+Definition apply_1 : Vector 2 -> GenPauliOp -> Vector 2 :=
+  fun psi op => (p1g_int op) × psi.
+
+Check is_action.
+
+Lemma mult_phase_comp: forall a b, phase_int (a) * phase_int (b) = 
+  phase_int (mult_phase a b).
+Admitted.
+
+Fact act_1_is_action:
+  is_action _ _ _ apply_1.
+Proof.
+  rewrite /is_action => x Hwf.
+  split.
+  {
+    rewrite /act_id /apply_1 /=.
+    lma'.
+  }
+  {
+    rewrite /act_comp /apply_1 => a b.
+    case a; case b => sa pa sb pb.
+    rewrite /p1g_int /=.
+    rewrite !Mscale_mult_dist_l Mscale_mult_dist_r Mscale_assoc.
+    rewrite -!mult_phase_comp.
+    rewrite Cmult_comm.
+    rewrite -!Mmult_assoc.
+    case sa; case sb; simpl; Qsimpl.
+    all: autorewrite with C_db.
+    all: case pa; case pb; simpl.
+    all: Qsimpl.
+    all: try easy.
+    all: solve_matrix.
+  }
+Qed.
+
+Canonical act_1 := Action _ _ _ _ act_1_is_action.
+
+(* I don't know how to make param implicit *)
+Goal act _ _ _ act_1 ∣0⟩ (% X) = ∣1⟩.
+Proof.
+  rewrite /= /apply_1 /=.
+  lma'.
+Qed.
+
+End QuantumActions.
 
 
